489048b00d07938decb380fa4a6aaaaf6d90dee0
Kevin Downs <kevin.downs04@gmail.com>
2019-11-14 16:49:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
 
+class CashRegister
+  
+  attr_accessor :total, :discount, :total_minus_last
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(item, price, quantity = 1)
+    @total_minus_last = @total
+    @total = @total += (price * quantity)
+  end
+  
+  def apply_discount
+    @total = @total *= (@discount.to_f * 0.01)
+    @total = @total.to_i
+  end
+  
+end
